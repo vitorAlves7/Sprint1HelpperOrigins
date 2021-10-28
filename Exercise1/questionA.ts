e92e53fc7183e1195fc8201afcba577a25db494b
vitorAlves7 <vitor.alves07750@outlook.com>
2021-10-28 12:42:44 -0300

    added comments and formatted hidentation

--- Exercise1/questionA.ts
+++ Exercise1/questionA.ts
@@ -1,16 +1,23 @@
-    let allVowels: string[] = ['a','e','i','o','u','A','E','I','O','U'];
+//All vowels,upper and lowercase
+let allVowels: string[] = ['a','e','i','o','u','A','E','I','O','U'];
 
-    function numberOfVowels(valueWord: string) : number{
-        
-        let allVowelsInTheStting: number = 0;
+/*
+    This function returns the number of vowels in a string.
+    Params: 1;
+    Type: string;
+    return: number of vowels; 
+*/
+function numberOfVowels(valueWord: string) : number{
+    let allVowelsInTheStting: number = 0;
 
-        for(let letter of valueWord){
-           if(allVowels.indexOf(letter) !== -1){
-               allVowelsInTheStting++;
-           }
+    for(let letter of valueWord){
+        if(allVowels.indexOf(letter) !== -1){
+            allVowelsInTheStting++;
         }
-        
-        return allVowelsInTheStting;
     }
+    
+    return allVowelsInTheStting;
+}
 
-    console.log(numberOfVowels('Winter is coming!'));
+
+console.log(numberOfVowels('Winter is coming!'));
